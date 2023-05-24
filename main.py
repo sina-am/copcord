57ab3b57c5ae92a5ce598d13a6f8fec578620e6c
sina <sina@techchi.ir>
2023-05-24 17:21:05 +0430

    first commit

---
+++ main.py
@@ -0,0 +1,20 @@
+import random
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+df = pd.read_csv('data/data-v0.2.csv', low_memory=False)
+
+for column in df.columns:
+    if column == 'LBP':
+        continue
+
+    df = df[df[column] != ' ']
+    df[column] = df[column].astype(np.float64)
+    df[column] = df[column].fillna(df[column].mode()[0])
+    
+    corr = df[column].corr(df['LBP'])
+    if (corr>0.2) or (corr< -0.2):
+        print(
+            f'Column name {column}, corr: {corr}')
+            
