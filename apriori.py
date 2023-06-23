fe8f805f1f895ec416efc3c2934196720237ae70
Sina Aarabi <sinaaarabi2@gmail.com>
2023-06-23 10:15:09 +0330

    update apriori

--- apriori.py
+++ apriori.py
@@ -1,43 +1,52 @@
-from mlxtend.preprocessing import TransactionEncoder
-from mlxtend.frequent_patterns import apriori, association_rules
 import pandas as pd
 import numpy as np
+from loader import load_data
+from mlxtend.preprocessing import TransactionEncoder
+from mlxtend.frequent_patterns import apriori, association_rules
 
-df = pd.read_csv('data-v1-with-NaN.CSV', low_memory=False,dtype=np.float64)
+df = load_data()
+
+df = df[
+    ['marital', 'a3', 'education',
+     'MS.prob', 'shoulder', 'elbow',
+     'wrist', 'hand', 'hip',
+     'knee', 'ankle', 'foot.finger',
+     'neck', 'vertebral', 'tra.history',
+     'past.MS.prb', 'shoulder.past',
+     'shld.pain.first.time', 'shld.pain.past',
+     'elbow.past', 'elb.pain.first.time', 'wrist.past',
+     'wr.pain.first.time', 'hand.pst', 'ha.pain.first.time',
+     'hip.past', 'hip.pain.first.time', 'knee.past', 'kn.pain.first.time',
+     'ankle.past', 'ank.pain.first.time', 'foot.finger.problem.past',
+     'ff.pain.past', 'ff.pain.first.time', 'neck.past', 'ne.pain.first.time',
+     'vertebral.past', 'ver.pain.first.time',
+     'disability', 'clothing', 'standing', 'drinking', 'eating', 'walking',
+     'KneeOA', 'HipOA', 'HandOA', 'CMC1OA', 'NeckOA', 'TotalOA', 'Hypermobility',
+     'RA', 'Behcet', 'SLE', 'fibromyalgia', 'Golfelbow', 'Tenniselbow', 'dequrvan',
+     'triggerfinger', 'spondyloarthropathy', 'Gout', 'LBP', 'lumbarradicolopathy',
+     'CTS', 'Chondromalaciapatella', 'FrozenShoulder', 'Rotatorcuff',
+     'periarthritis', 'neckradicolopathy'
+     ]]
 
 df.fillna(round(df.mean()), inplace=True)
 
-column_names = list(df.columns.values)
+transactions = np.zeros(len(df), dtype=np.ndarray)
+for row_index, row in enumerate(df.values.tolist()):
+    item_sets = []
+    for i, item in enumerate(row):
+        if item != 0:
+            item_sets.append(f'{df.columns[i]}.{int(item)}')
 
-for column in column_names:
-    column_values = df[column].unique()
-    num_values = len(column_values)
-
-    if num_values == 2:
-        bins = 2
-    elif num_values == 3:
-        bins = 3
-    else:
-        bins = 4
-
-    # Convert column values to intervals
-    df[column] = pd.cut(df[column], bins=bins)
-
-    # Convert intervals to categorical data and encode as integers
-    df[column] = pd.Categorical(df[column])
-    df[column] = df[column].cat.codes
-  
-
-data_bin = df.astype(int)
-data_transactions = data_bin.values.tolist()
-
+    transactions[row_index] = np.array(item_sets)
 
 te = TransactionEncoder()
-Transaction_Array = te.fit(data_transactions).transform(data_transactions)
-df = pd.DataFrame(Transaction_Array, columns=te.columns_)
-frequent_itemsets = apriori(df, min_support=0.3, use_colnames=True)
+Transaction_Array = te.fit(transactions).transform(transactions)
 
-rules = association_rules(frequent_itemsets, metric="confidence", min_threshold=0.8)
+new_df = pd.DataFrame(Transaction_Array, columns=te.columns_)
 
-print(frequent_itemsets)
+frequent_items = apriori(new_df, min_support=0.3, use_colnames=True)
+print(frequent_items)
+
+rules = association_rules(frequent_items, metric="confidence", min_threshold=0.9)
+
 print(rules)
