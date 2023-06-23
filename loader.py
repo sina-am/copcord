557c0c6bb0aa4313540510bb8ebdbbb33696e41c
Sina Aarabi <sinaaarabi2@gmail.com>
2023-06-23 10:16:35 +0330

    move svm to note file, add clustering

--- loader.py
+++ loader.py
@@ -1,34 +1,25 @@
+import os
 import pandas as pd
 import numpy as np
 
 
-def is_float(value: str):
-    try:
-        float(value)
-    except ValueError:
-        return False
-    return True
-
-
-def clean_data(df: pd.DataFrame) -> pd.DataFrame:
-    df = df.replace(' ', np.NaN)
-    df['age'] = df['age'].astype(np.float64)
-    df['birthdat'] = df['birthdat'].astype(np.float64)
-    df['LBP'] = df['LBP'].astype(np.float64)
-    df['KneeOA'] = df['KneeOA'].astype(np.float64)
-    df['len'] = df['len'].astype(np.float64)
-    df['weight'] = df['weight'].astype(np.float64)
-    df['Agegroup'] = df['Agegroup'].astype(np.float64)
-    df['ank.tenderness'] = df['ank.tenderness'].astype(np.float64)
-    df['kn.pain'] = df['kn.pain'].astype(np.float64)
-    df['knee'] = df['knee'].astype(np.float64)
-    return df
-
-
-def load_data(path: str) -> pd.DataFrame:
+def clean_data():
     df = pd.read_csv('data/data.csv', low_memory=False)
-    return clean_data(df)
+    df.drop(['intdate', 'intstart', 'intend', 'code'], axis=1, inplace=True)
+
+    df.replace(' ', np.NaN, inplace=True)
+    cols_to_delete = df.columns[df.isnull().sum()/len(df) > 0.1]
+    df.drop(cols_to_delete, axis=1, inplace=True)
+
+    for column in df.columns:
+        df[column] = df[column].astype('str')
+        df = df[~df[column].str.contains('?', regex=False)]
+
+    df.to_csv('data/data-v0.1.csv')
 
 
-if __name__ == '__main__':
-    load_data('data/data.csv')
+def load_data() -> pd.DataFrame:
+    if not os.path.exists('data/data-v0.1.csv'):
+        clean_data()
+
+    return pd.read_csv('data/data-v0.1.csv', low_memory=False)
